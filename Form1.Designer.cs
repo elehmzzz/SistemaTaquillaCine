ca3ec422a17034ac6298ee63216ef8bed88efce7
elehmzzz <elehmzzz@gmail.com>
2026-06-24 15:29:57 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SistemaTaquillaCine
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbmTipoBoleto = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCosto = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblExtra = new System.Windows.Forms.Label();
+            this.txtExtra = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtExtra);
+            this.panel1.Controls.Add(this.lblExtra);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblCosto);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cbmTipoBoleto);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 298);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo de boleto:";
+            // 
+            // cbmTipoBoleto
+            // 
+            this.cbmTipoBoleto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbmTipoBoleto.FormattingEnabled = true;
+            this.cbmTipoBoleto.Items.AddRange(new object[] {
+            "General",
+            "Adulto Mayor",
+            "Estudiante"});
+            this.cbmTipoBoleto.Location = new System.Drawing.Point(19, 65);
+            this.cbmTipoBoleto.Name = "cbmTipoBoleto";
+            this.cbmTipoBoleto.Size = new System.Drawing.Size(215, 37);
+            this.cbmTipoBoleto.TabIndex = 1;
+            this.cbmTipoBoleto.SelectedIndexChanged += new System.EventHandler(this.cbmTipoBoleto_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TAQUILLA DE CINE";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(760, 78);
+            this.panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Costo:";
+            // 
+            // lblCosto
+            // 
+            this.lblCosto.AutoSize = true;
+            this.lblCosto.Location = new System.Drawing.Point(394, 20);
+            this.lblCosto.Name = "lblCosto";
+            this.lblCosto.Size = new System.Drawing.Size(27, 30);
+            this.lblCosto.TabIndex = 3;
+            this.lblCosto.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Detalles:";
+            // 
+            // lblExtra
+            // 
+            this.lblExtra.AutoSize = true;
+            this.lblExtra.Location = new System.Drawing.Point(14, 195);
+            this.lblExtra.Name = "lblExtra";
+            this.lblExtra.Size = new System.Drawing.Size(22, 30);
+            this.lblExtra.TabIndex = 5;
+            this.lblExtra.Text = "-";
+            this.lblExtra.Visible = false;
+            // 
+            // txtExtra
+            // 
+            this.txtExtra.Location = new System.Drawing.Point(19, 247);
+            this.txtExtra.Name = "txtExtra";
+            this.txtExtra.Size = new System.Drawing.Size(100, 39);
+            this.txtExtra.TabIndex = 6;
+            this.txtExtra.Visible = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 457);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmPrincipal";
+            this.Text = "Sistema de Taquilla de Cine";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbmTipoBoleto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCosto;
+        private System.Windows.Forms.Label lblExtra;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExtra;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaTaquillaCine
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        //metodo privado que carga el formulario
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            cbmTipoBoleto.SelectedIndex = 0;
+            lblCosto.Text = "50.00";
+        }
+
+        private void tipoBoleto() {
+            int indice = cbmTipoBoleto.SelectedIndex;
+            switch (indice) {
+                case 0:
+                    //general
+                    lblExtra.Visible = false;
+                    txtExtra.Visible = false;
+                    break;
+                case 1:
+                    //adulto mayor
+                    lblExtra.Visible = true;
+                    lblExtra.Text = "No. del INAPAM:";
+                    txtExtra.Visible = true;
+                    break;
+                case 2:
+                    //estudiante
+                    lblExtra.Visible = true;
+                    lblExtra.Text = "Matricula:";
+                    txtExtra.Visible = true;
+                    break;
+            }
+        }
+        private void cbmTipoBoleto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tipoBoleto();
+        }
+    }
+}
