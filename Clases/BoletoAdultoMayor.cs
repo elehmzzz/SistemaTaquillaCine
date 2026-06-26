e0afa954abc2903beb07bcb650fa956ba4ed33b6
Hermelinda.Cruz <20251066@uthh.edu.mx>
2026-06-25 23:22:53 -0500

    se agrego a la clase y se modifico

--- Clases/BoletoAdultoMayor.cs
+++ Clases/BoletoAdultoMayor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SistemaTaquillaCine
+namespace SistemaTaquillaCine.Clases
 {
     internal class BoletoAdultoMayor: clsBoleto
     {
