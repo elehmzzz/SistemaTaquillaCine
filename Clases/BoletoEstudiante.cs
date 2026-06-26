45d9c0c6f6b890bf23ddd0a5c6c5b045f8360e7f
elehmzzz <elehmzzz@gmail.com>
2026-06-26 08:40:46 -0600

    Se implementó las clases eliminadas: boletoEstudiante y boletoGeneral

---
+++ Clases/BoletoEstudiante.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTaquillaCine.Clases
+{
+    internal class BoletoEstudiante: clsBoleto
+    {
+        // Guarda la matrícula del estudiante
+        private string matricula;
+
+        // Permite ver y cambiar la matrícula desde fuera
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        // Constructor: guarda la matrícula y manda el precio a la clase padre
+        public BoletoEstudiante(float precioBase, string matricula) : base(precioBase)
+        {
+            this.matricula = matricula;
+        }
+
+        // Calcula el precio final (descuento del 30%)
+        public override float calcularPrecioFinal()
+        {
+            return precioBase * 0.70f;  // 70% del precio original
+        }
+
+        // Devuelve la información del boleto como texto
+        public override string ToString()
+        {
+            return "Boleto Estudiante - Matrícula: " + matricula + " - Precio: $" + calcularPrecioFinal();
+        }
+    }
+}
---
+++ Clases/BoletoGeneral.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTaquillaCine.Clases
+{
+    internal class BoletoGeneral:clsBoleto
+    {
+        // El constructor ahora recibe y pasa un float a la clase base
+        public BoletoGeneral(float precioBase) : base(precioBase)
+        {
+            // No requiere atributos adicionales
+        }
+        public override float calcularPrecioFinal()
+        {
+            // Retorna la variable protegida de la clase padre
+            return precioBase;
+        }
+    }
+}
