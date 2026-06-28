d6615adac6e41a54b463558266e74241a9e29ad0
elehmzzz <elehmzzz@gmail.com>
2026-06-28 00:45:39 -0600

    se modificó la interfaz principal y se agrego la lógica de funcionamiento, usando el boton calcular.

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -29,24 +29,32 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalPagar = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtExtra = new System.Windows.Forms.TextBox();
+            this.lblExtra = new System.Windows.Forms.Label();
+            this.lblCosto = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.cbmTipoBoleto = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lblCosto = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lblExtra = new System.Windows.Forms.Label();
-            this.txtExtra = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lblTotalPagar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblDescuento);
+            this.panel1.Controls.Add(this.btnCalcular);
             this.panel1.Controls.Add(this.txtExtra);
             this.panel1.Controls.Add(this.lblExtra);
-            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.lblCosto);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.cbmTipoBoleto);
@@ -54,17 +62,90 @@
             this.panel1.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(12, 96);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(760, 298);
+            this.panel1.Size = new System.Drawing.Size(760, 349);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // lblTotalPagar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tipo de boleto:";
+            this.lblTotalPagar.AutoSize = true;
+            this.lblTotalPagar.Location = new System.Drawing.Point(246, 286);
+            this.lblTotalPagar.Name = "lblTotalPagar";
+            this.lblTotalPagar.Size = new System.Drawing.Size(27, 30);
+            this.lblTotalPagar.TabIndex = 11;
+            this.lblTotalPagar.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total a pagar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Descuento:";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(246, 225);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(27, 30);
+            this.lblDescuento.TabIndex = 8;
+            this.lblDescuento.Text = "0";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(550, 278);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(187, 45);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtExtra
+            // 
+            this.txtExtra.Location = new System.Drawing.Point(346, 63);
+            this.txtExtra.Name = "txtExtra";
+            this.txtExtra.Size = new System.Drawing.Size(138, 39);
+            this.txtExtra.TabIndex = 6;
+            this.txtExtra.Visible = false;
+            // 
+            // lblExtra
+            // 
+            this.lblExtra.AutoSize = true;
+            this.lblExtra.Location = new System.Drawing.Point(350, 20);
+            this.lblExtra.Name = "lblExtra";
+            this.lblExtra.Size = new System.Drawing.Size(22, 30);
+            this.lblExtra.TabIndex = 5;
+            this.lblExtra.Text = "-";
+            this.lblExtra.Visible = false;
+            // 
+            // lblCosto
+            // 
+            this.lblCosto.AutoSize = true;
+            this.lblCosto.Location = new System.Drawing.Point(246, 161);
+            this.lblCosto.Name = "lblCosto";
+            this.lblCosto.Size = new System.Drawing.Size(27, 30);
+            this.lblCosto.TabIndex = 3;
+            this.lblCosto.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio estandar:";
             // 
             // cbmTipoBoleto
             // 
@@ -79,6 +160,15 @@
             this.cbmTipoBoleto.Size = new System.Drawing.Size(215, 37);
             this.cbmTipoBoleto.TabIndex = 1;
             this.cbmTipoBoleto.SelectedIndexChanged += new System.EventHandler(this.cbmTipoBoleto_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo de boleto:";
             // 
             // label2
             // 
@@ -98,51 +188,6 @@
             this.panel2.Size = new System.Drawing.Size(760, 78);
             this.panel2.TabIndex = 2;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(301, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 30);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Costo:";
-            // 
-            // lblCosto
-            // 
-            this.lblCosto.AutoSize = true;
-            this.lblCosto.Location = new System.Drawing.Point(394, 20);
-            this.lblCosto.Name = "lblCosto";
-            this.lblCosto.Size = new System.Drawing.Size(27, 30);
-            this.lblCosto.TabIndex = 3;
-            this.lblCosto.Text = "0";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 140);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(112, 30);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Detalles:";
-            // 
-            // lblExtra
-            // 
-            this.lblExtra.AutoSize = true;
-            this.lblExtra.Location = new System.Drawing.Point(14, 195);
-            this.lblExtra.Name = "lblExtra";
-            this.lblExtra.Size = new System.Drawing.Size(22, 30);
-            this.lblExtra.TabIndex = 5;
-            this.lblExtra.Text = "-";
-            this.lblExtra.Visible = false;
-            // 
-            // txtExtra
-            // 
-            this.txtExtra.Location = new System.Drawing.Point(19, 247);
-            this.txtExtra.Name = "txtExtra";
-            this.txtExtra.Size = new System.Drawing.Size(100, 39);
-            this.txtExtra.TabIndex = 6;
-            this.txtExtra.Visible = false;
-            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -152,7 +197,6 @@
             this.Controls.Add(this.panel1);
             this.Name = "frmPrincipal";
             this.Text = "Sistema de Taquilla de Cine";
-            this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -171,8 +215,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblCosto;
         private System.Windows.Forms.Label lblExtra;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtExtra;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblTotalPagar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
 
